5baae27d41940f28535094adfdecc013aebd76dc
Joelle Gilley <gilley.joelle@gmail.com>
2016-07-16 10:54:33 -0400

    SQS Test

--- www/index.php
+++ www/index.php
@@ -1,1 +1,18 @@
-<?php phpinfo();
+<?php
+
+    require 'vendor/autoload.php';
+
+
+    // No credentials to force SDK to use STS for EC2 IAM Roles
+    $sqs_credentials = array();
+
+    // Instantiate the client
+    $sqs_client = new SqsClient($sqs_credentials);
+
+    $response = $sqsi_client->listQueues();
+    print_r($response);
+
+
+
+
+
